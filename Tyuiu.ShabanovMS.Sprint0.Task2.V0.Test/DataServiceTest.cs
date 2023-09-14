c95559f53e107617c45ee5a5771460f4ac3b3efb
Tyuiu.ShabanovMS@outlook.com <shabanov2014@bk.ru>
2023-09-15 00:21:24 +0500

    Add Task2

---
+++ Tyuiu.ShabanovMS.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.ShabanovMS.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.ShabanovMS.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            //Область создания методов тестирования, методов из библиотеки
+            var name = "Мирон";
+            var res = DataService.GetMessage(name);
+
+            //Вызываем класс Assert и метод AreEqual
+            Assert.AreEqual("Привет..., Мирон", res);
+        }
+    }
+}
---
+++ Tyuiu.ShabanovMS.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.ShabanovMS.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.ShabanovMS.Sprint0.Task2.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Вызов класса DataService и метода GetMassage из библиотеки Tyuiu.ShabanovMS.Sprint0.Task2.V0.Lib
+            Console.WriteLine(DataService.GetMessage("Мирон"));
+            Console.ReadKey();
+        }
+    }
+}
